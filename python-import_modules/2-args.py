f77ddf6da473e985205288550c907be62f407754
FabioH7 <fhysollari4@gmail.com>
2023-01-18 04:02:37 -0800

    fixing initial print

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,12 @@
 import sys
 if __name__ == "__main__":
     i = 1
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
     while i < len(sys.argv):
         print("{}: {}".format(i, sys.argv[i]))
         i += 1
