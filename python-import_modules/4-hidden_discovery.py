a86d2b9a8c3064777d520f08ee26b8a242275329
FabioH7 <fhysollari4@gmail.com>
2023-01-18 04:38:29 -0800

    fixing file

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    for item in dir('hidden_4.pyc'):
-        if item[0] != "_":
+    import hidden_4
+
+    x = "__"
+    for item in dir(hidden_4):
+        if x not in item:
             print(item)
