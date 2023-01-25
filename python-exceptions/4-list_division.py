fdc5f1e9072a433e284842c47226a8497c3cf18d
FabioH7 <fhysollari4@gmail.com>
2023-01-25 02:01:11 -0800

    adding list division func

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,11 +9,12 @@ def list_division(my_list_1, my_list_2, list_length):
             except ZeroDivisionError:
                 print("division by 0")
                 new_list.append(0)
-            except ValueError:
+            except TypeError:
                 print("wrong type")
                 new_list.append(0)
-    except IndexError:
-        print("out of range")
-        new_list.append(0)
+            except IndexError:
+                print("index out of range")
+                new_list.append(0)
+            i += 1
     finally:
         return new_list
