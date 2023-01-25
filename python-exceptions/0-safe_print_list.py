558ddae19e6889eef9332517e62c5728eff0a2bc
FabioH7 <fhysollari4@gmail.com>
2023-01-25 00:47:53 -0800

    fixing printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,10 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         while i < x:
-            if i + 1 == x:
-                print("{}".format(my_list[i]))
-            else:
-                print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             i += 1
     except IndexError:
         print()
         return i
+    print()
     return i
