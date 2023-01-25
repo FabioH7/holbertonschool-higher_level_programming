ed36cfbc7bbbc6150d1e4c3dbed8b9ecb531e78f
FabioH7 <fhysollari4@gmail.com>
2023-01-25 01:03:59 -0800

    safe print division func

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        return result
