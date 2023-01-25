78dbe00a1dd5bc79adc18a013cb5ebaacf358b97
FabioH7 <fhysollari4@gmail.com>
2023-01-25 01:20:26 -0800

    list division no errors func

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,4 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
     except IndexError:
         print("out of range")
         new_list.append(0)
-    return new_list
+    finally:
+        return new_list
