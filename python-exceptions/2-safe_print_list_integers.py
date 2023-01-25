191bdd9e16262366a8faa48df4747a6481b05188
FabioH7 <fhysollari4@gmail.com>
2023-01-25 00:50:36 -0800

    fixing index error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        i = 0
-        while i < x:
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                i += 1
-                count += 1
-            except ValueError:
-                i += 1
-                continue
-            except TypeError:
-                i += 1
-                continue
+    i = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+            count += 1
+        except ValueError:
+            i += 1
+            continue
+        except TypeError:
+            i += 1
+            continue
     print()
     return count
