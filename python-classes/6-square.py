554372a372ee738da2e21721359334e6b4f70b23
FabioH7 <fhysollari4@gmail.com>
2023-01-30 05:33:53 -0800

    adding final position

---
+++ python-classes/6-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""another square module """
+
+
+class Square:
+    '''init func'''
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        if type(position) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__size = size
+        self.__position = position
+
+    '''getter for size'''
+    @property
+    def size(self):
+        return self.__size
+
+    '''change size func'''
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+    '''find area of square'''
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for height in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for width in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                if j + 1 == self.__size:
+                    print('#')
+                else:
+                    print('#', end="")
+        if self.__size == 0:
+            print()
