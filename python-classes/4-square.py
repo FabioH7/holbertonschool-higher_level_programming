dc17f00b621730180530596c8b59f7bec97c907a
FabioH7 <fhysollari4@gmail.com>
2023-01-30 04:59:20 -0800

    adding setters and getter

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""another square module """
+
+
+class Square:
+    '''init func'''
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    '''getter for size'''
+    @property
+    def size(self):
+        return self.__size
+
+    '''change size func'''
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    '''find area of square'''
+    def area(self):
+        return self.__size ** 2
