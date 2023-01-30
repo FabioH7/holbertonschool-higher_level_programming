5773c837b017ee82a7b79b8d1ff7a133bb020437
FabioH7 <fhysollari4@gmail.com>
2023-01-30 04:22:54 -0800

    raising errors

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""another square module """
+
+
+class Square:
+    '''init func'''
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
