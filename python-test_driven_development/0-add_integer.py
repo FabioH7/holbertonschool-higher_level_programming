8f74bcdf6f262cb515f99f0aa678b18ed2d51d65
FabioH7 <fhysollari4@gmail.com>
2023-01-31 01:26:29 -0800

    first test modules

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" Integer addition module """
+
+
 def add_integer(a, b=98):
+    """ Returns addition of a and b
+
+    raises TypeError if a or b are not integers or floats
+    """
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
