7207d9523b52365d1bb66faf77dc27641f673276
FabioH7 <fhysollari4@gmail.com>
2023-01-31 02:52:45 -0800

    indented text

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError('text must be a string')
     char = ['.', '?', ':']
     i = 0
     while i in range(len(text)):
         print('{}'.format(text[i]), end="")
         if text[i] in char:
             print('\n')
-            if text[i + 1] == " ":
+            while text[i + 1] == " ":
                 i += 1
         i += 1
