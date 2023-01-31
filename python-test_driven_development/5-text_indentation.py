78129d8455ef6e6a8c62de1d2e2dc98b719499de
FabioH7 <fhysollari4@gmail.com>
2023-01-31 02:47:27 -0800

    printing indented texts

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    char = ['.', '?', ':']
+    i = 0
+    while i in range(len(text)):
+        print('{}'.format(text[i]), end="")
+        if text[i] in char:
+            print('\n')
+            i += 1
+        i += 1
