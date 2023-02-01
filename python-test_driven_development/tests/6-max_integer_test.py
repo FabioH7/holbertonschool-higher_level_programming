5ba8111d87ac5152e23d5e8e35a889995bb63c00
FabioH7 <fhysollari4@gmail.com>
2023-02-01 03:24:37 -0800

    adding all tests for unit tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,3 +8,8 @@ class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertAlmostEqual(max_integer([1, 2, 5, 4]), 5)
+        self.assertAlmostEqual(max_integer([6, 2, 5, 4]), 6)
+        self.assertAlmostEqual(max_integer([1, 2, -3, -1]), 2)
+        self.assertAlmostEqual(max_integer([-5, -7, -2, -4]), -2)
+        self.assertAlmostEqual(max_integer([4]), 4)
+        self.assertAlmostEqual(max_integer(), None)
