bee016b10f364ce8826f117830fde287c04f5bef
FabioH7 <fhysollari4@gmail.com>
2023-02-01 02:59:28 -0800

    adding module and function comments

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Text indentation module"""
+
+
 def text_indentation(text):
+    """Indents text whenever needed"""
     if type(text) is not str:
         raise TypeError('text must be a string')
     char = ['.', '?', ':']
