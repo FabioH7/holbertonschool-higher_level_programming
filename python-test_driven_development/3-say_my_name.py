6add024458d09404678ab9d83ff0a1c926f54118
FabioH7 <fhysollari4@gmail.com>
2023-02-01 02:55:54 -0800

    fixing module comments

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Say my name Module """
+
 
 def say_my_name(first_name, last_name=""):
+    """Gets a name and a surname if provided and prints the full name"""
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')
     if type(last_name) is not str:
