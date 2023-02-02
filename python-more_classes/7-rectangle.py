532b2ccd32bd3777b519af2884cb676065ab57b3
FabioH7 <fhysollari4@gmail.com>
2023-02-02 03:11:17 -0800

    adding print_symbol class attribute

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
             return string
         for i in range(self.__height):
             for j in range(self.__width):
-                string += Rectangle.print_symbol
+                string += str(self.print_symbol)
             if i + 1 != self.__height:
                 string += '\n'
         return string
