a38be7b85353a83cae3f5036f1e7451cfc7d1dcc
FabioH7 <fhysollari4@gmail.com>
2023-02-06 04:02:20 -0800

    adding function

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-'''Copy list module'''
-
-
 def copy_list(a_list):
-    new_list = []
-    for elem in a_list:
-        new_list.append(elem)
-    return new_list
+    return a_list.copy()
