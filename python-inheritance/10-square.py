fd601b45f4efd5608e4744ee5914f551dad0bfe4
FabioH7 <fhysollari4@gmail.com>
2023-02-07 07:31:30 -0800

    adding square class

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""Module for square class"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""Module for square class"""
 
 
 class Square(Rectangle):
     '''Subclass of Rectangle named square'''
     def __init__(self, size):
+        '''Initialisation of square object '''
         BaseGeometry.integer_validator(self, 'size', size)
         Rectangle.__init__(self, size, size)
         self.__size = size
 
     def area(self):
+        '''Returns area of square'''
         return self.__size ** 2
