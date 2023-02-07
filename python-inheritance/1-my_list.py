5dfa0e1543ff92df62d00e5ec7e40168baff01a3
FabioH7 <fhysollari4@gmail.com>
2023-02-07 02:04:48 -0800

    adding subclass method

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for a list subclass"""
+
+
+class MyList(list):
+    '''Prints a sorted list'''
+    def print_sorted(self):
+        ls = self.copy()
+        ls.sort()
+        print(ls)
