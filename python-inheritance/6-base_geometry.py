e0488e0308fa7a421ae6f9c4003aa7ef9eec0ca0
FabioH7 <fhysollari4@gmail.com>
2023-02-07 02:55:44 -0800

    adding empty method

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -4,6 +4,8 @@
 
 class BaseGeometry:
     '''Geometry class'''
+    def __init__(self):
+        pass
     def area(self):
-        raise Exception(area() is not implemented)
+        raise Exception('area() is not implemented')
     pass
