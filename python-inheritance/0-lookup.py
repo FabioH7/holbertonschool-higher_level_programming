168e05f1205162f21042e584d01d968327a632e5
FabioH7 <fhysollari4@gmail.com>
2023-02-07 01:11:49 -0800

    adding lookup function

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for lookup function'''
+
+
+def lookup(obj):
+    '''Function that looks up all for available methods
+    and attributes of an object'''
+    return dir(obj)
