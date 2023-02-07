1b1cfe488febe7e937c43f4414740fb548fc2fff
FabioH7 <fhysollari4@gmail.com>
2023-02-07 08:52:25 -0800

    adding write json to file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module to write in file"""
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    '''Writes in json string format'''
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(my_obj))
