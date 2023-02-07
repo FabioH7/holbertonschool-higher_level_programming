ef213bf1b77c8d408f02e857008996a0468133c3
FabioH7 <fhysollari4@gmail.com>
2023-02-07 08:44:13 -0800

    adding json dumps

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module to write in file"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    '''Writes in json string format'''
+    return json.dumps(my_obj)
+    return count
