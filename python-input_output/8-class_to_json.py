2eb6f108c275f9d0815f99c0f34671f660ab53c0
FabioH7 <fhysollari4@gmail.com>
2023-02-08 00:29:01 -0800

    adding class to json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,10 +2,8 @@
 """Class to json Module"""
 
 
-import json
-
-
 def class_to_json(obj):
+    """Class to json func"""
     d = {}
     for attr, value in obj.__dict__.items():
         d[attr] = value
