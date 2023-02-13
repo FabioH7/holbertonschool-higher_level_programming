6add6b1c1182cd1a31bd84254f59e5455bb2694f
FabioH7 <fhysollari4@gmail.com>
2023-02-13 08:15:53 -0800

    adding tostr

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,6 +5,19 @@
 from models.rectangle import Rectangle
 
 
+def raise_exception(**kwargs):
+    """Raises exceptions for rectangle class"""
+    sides = ['width', 'height']
+    ls = ['x', 'y']
+    for key, value in kwargs.items():
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(key))
+        if key in sides and value <= 0:
+            raise ValueError("{} must be > 0".format(key))
+        if key in ls and value < 0:
+            raise ValueError("{} must be >= 0".format(key))
+
+
 class Square(Rectangle):
     """Square class, inherits from rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
@@ -16,3 +29,13 @@ class Square(Rectangle):
         tostr += "{}/{} - ".format(self.x, self.y)
         tostr += "{}".format(self.size)
         return tostr
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        raise_exception(width=size)
+        self.width = size
+        self.height = size
