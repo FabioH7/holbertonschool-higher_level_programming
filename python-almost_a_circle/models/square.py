990c834270cd3de78a00b9f837a419434e12d580
FabioH7 <fhysollari4@gmail.com>
2023-02-13 07:40:49 -0800

    addinf new square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for square class"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class, inherits from rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        Rectangle.__init__(self, size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        tostr = "[Square] ({})".format(self.id)
+        tostr += "{}/{} - ".format(self.x, self.y)
+        tostr += "{}".format(self.size)
+        return tostr
