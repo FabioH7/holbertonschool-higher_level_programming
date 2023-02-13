64fd33d02e180ff9ace3879d697be0ff20c4c61c
FabioH7 <fhysollari4@gmail.com>
2023-02-13 09:54:06 -0800

    adding square method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -37,3 +37,14 @@ class Square(Rectangle):
     def size(self, size):
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """Updates square object attributes"""
+        att = ['id', 'size', 'x', 'y']
+        if len(args) != 0:
+            for i in range(len(args)):
+                setattr(self, att[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
