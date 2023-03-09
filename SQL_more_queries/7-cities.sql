fc95ddbf531c06c41033d3fbf478bb0c13c69c7a
FabioH7 <fhysollari4@gmail.com>
2023-03-09 02:36:05 -0800

    adding cities tab;e

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- create database and cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id));
