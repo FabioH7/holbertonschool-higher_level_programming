625747e9f06fbd458a68014cf7967b66814777ae
FabioH7 <fhysollari4@gmail.com>
2023-03-13 05:55:53 -0700

    adding state filter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,9 +8,11 @@ if __name__ == '__main__':
     u = sys.argv[1]
     p = sys.argv[2]
     d = sys.argv[3]
-    db = MySQLdb.connect(host='localhost', port=3306, user=u, passwd=p, db=d)
+    db = MySQLdb.connect(host='localhost', port=3306, user=u, passwd=p, db=d,
+                         charset='utf8')
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
