901256c33949303069b78ca03a4216a4c03a4df0
FabioH7 <fhysollari4@gmail.com>
2023-03-13 10:34:23 -0700

    adding model state]

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python3
+"""model state class"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Sequence, String
 
 
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'states'
