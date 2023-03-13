6de3ac3cb5dae77938f4e824f2d47ad1bcd5a434
FabioH7 <fhysollari4@gmail.com>
2023-03-13 05:44:49 -0700

    adding search by state name

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     d = sys.argv[3]
     db = MySQLdb.connect(host='localhost', port=3306, user=u, passwd=p, db=d)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
