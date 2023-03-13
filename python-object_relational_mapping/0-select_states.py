39a2bdb659c15ff078eb430cfa669da4191313c2
FabioH7 <fhysollari4@gmail.com>
2023-03-13 05:11:57 -0700

    adding select states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,14 @@ import sys
 import MySQLdb
 ''' Get all rows of a table '''
 
-
-r = sys.argv[1]
-p = sys.argv[2]
-db = MySQLdb.connect(host='localhost', user=r, passwd=p, db=sys.argv[3])
-cur = db.cursor()
-cur.execute('SELECT * FROM states')
-states = cur.fetchall()
-for state in states:
-    print(state)
-cur.close()
-db.close()
+if __name__ = '__main__':
+    r = sys.argv[1]
+    p = sys.argv[2]
+    db = MySQLdb.connect(host='localhost', user=r, passwd=p, db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states')
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
