fcf296f41738001bd75abae4adefd5601503ab14
FabioH7 <fhysollari4@gmail.com>
2023-03-13 04:52:12 -0700

    adding select * states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 '''get all rows of a table'''
+
+
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+r = sys.argv[1]
+p = sys.argv[2]
+db = MySQLdb.connect(host='localhost', user=r, passwd=p, db=sys.argv[3])
 cur = db.cursor()
 cur.execute('SELECT * FROM states')
 states = cur.fetchall()
