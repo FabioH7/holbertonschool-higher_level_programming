14841003d86c957accd0f7fb4d39362c714a997c
FabioH7 <fhysollari4@gmail.com>
2023-04-11 09:28:05 +0200

    finding index occurence

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let x = 0;
+  for (const index of list) {
+    if (index === searchElement) {
+      x++;
+    }
+  }
+  return x;
+};
