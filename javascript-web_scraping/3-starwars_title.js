d72fe0114ed8e95027b7b8aa930d8901acd5955c
FabioH7 <fhysollari4@gmail.com>
2023-04-18 09:39:38 +0200

    adding smth idk

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -8,5 +8,5 @@ request(api, function (error, response, body) {
   if (error) {
     console.error('error:', error); // Print the error if one occurred
   }
-  console.log('code:', response.body.title); // Print the response status code if a response was received
+  console.log('code:', response.body); // Print the response status code if a response was received
 });
