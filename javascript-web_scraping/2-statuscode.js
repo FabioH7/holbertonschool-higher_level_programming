b743eb1cde1de9e3c47d2047a54c12ff014f3250
FabioH7 <fhysollari4@gmail.com>
2023-04-18 09:22:27 +0200

    adding smth idk

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,8 @@ const process = require('process');
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  }
   console.log('code:', response && response.statusCode); // Print the response status code if a response was received
 });
