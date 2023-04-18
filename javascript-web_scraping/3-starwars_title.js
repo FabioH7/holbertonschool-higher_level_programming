cd1b96614a4222bca5b26f5c68cc954df7fc464c
FabioH7 <fhysollari4@gmail.com>
2023-04-18 09:35:13 +0200

    adding smth idk

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
 const process = require('process');
-const request = require('request');
+// const request = require('request');
+let api = "https://swapi-api.hbtn.io/api/films/" + process.argv[2]
+console.log(api)
 
-request(process.argv[2], function (error, response, body) {
+request(api, function (error, response, body) {
   if (error) {
     console.error('error:', error); // Print the error if one occurred
   }
-  console.log('code:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('code:', response.data.title); // Print the response status code if a response was received
 });
