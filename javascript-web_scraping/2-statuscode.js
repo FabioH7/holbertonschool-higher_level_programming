6db564212cebbd36e86559cf0118983076bed0d3
FabioH7 <fhysollari4@gmail.com>
2023-04-18 09:20:29 +0200

    adding smth idk

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
