38c7156e9ffed4dac805a684e6d9726873718878
FabioH7 <fhysollari4@gmail.com>
2023-04-18 13:41:08 +0200

    adding

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+const fs = require('fs');
+const api = process.argv[2];
+const filename = process.argv[3];
+let data = '';
+request(api, function (error, response, body) {
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  }
+  data = body;
+}).pipe(fs.writeFile(filename, data, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+}));
