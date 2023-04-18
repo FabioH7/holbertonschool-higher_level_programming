7714290487d746a233412df88ea86fdd57ebc418
FabioH7 <fhysollari4@gmail.com>
2023-04-18 10:23:37 +0200

    adding

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const api = 'https://swapi-api.hbtn.io/api/films/';
+
+request(api, function (error, response, body) {
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  }
+  const films = JSON.parse(response.body); // Print the response status code if a response was received
+  const filtered = films.results.filter(film => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/'));
+  return filtered.length;
+});
